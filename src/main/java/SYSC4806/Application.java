41ce9e013082969c26fb09d0f4b021b4185efc25
Kshamina <kshamina.ghelani@carleton.ca>
2019-03-04 11:26:14 -0500

    Fixed mappings

--- src/main/java/SYSC4806/Application.java
+++ src/main/java/SYSC4806/Application.java
@@ -27,39 +27,56 @@ public class Application {
     public CommandLineRunner demo(CategoryRepository categoryRepository, CourseRepository courseRepository, LearningOutcomeRepository learningOutcomeRepository, ProgramRepository programRepository) {
         return (args) -> {
 
-            // Testing purposes to connect DB to Front
-            // Can delete this setup once DB is up
-            // Hard Input of each object.
             Category category = new Category("Programming");
             Category category2 = new Category("Software Architecture");
             Category category3 = new Category("Logic Algorithm");
+
             LearningOutcome learningOutcome = new LearningOutcome("Web Application Coding", category);
+            LearningOutcome learningOutcome2 = new LearningOutcome("Design Software System", category2);
+            LearningOutcome learningOutcome3 = new LearningOutcome("Logic Gates", category3);
+
             Course course = new Course(4, "Software Engineering Lab", "4806");
-            Course course2 = new Course(3, "Real-Time Concurrent System", "1111");
-            Course course3 = new Course(3, "Operating Systems", "2222");
+            Course course2 = new Course(3, "Real-Time Concurrent System", "3303");
+            Course course3 = new Course(3, "Operating Systems", "4001");
+            Course course4 = new Course(2, "Circuits and Signals", "2501");
+            Course course5 = new Course(2, "Switching Circuits", "2607");
+            Course course6 = new Course(1, "Introduction to Software", "1005");
+            Course course7 = new Course(1, "Introduction to Engineering", "1010");
+
             Program program = new Program("Software Engineering");
             Program program2 = new Program("Civil Engineering");
             Program program3 = new Program("Computer System Engineering");
-
-            category.addLO(learningOutcome);
-            learningOutcome.addCourse(course);
+            Program program4 = new Program("Electrical Engineering");
 
             course.addLO(learningOutcome);
             course.addProgram(program);
+            course2.addLO(learningOutcome);
+            course2.addProgram(program);
+            course3.addLO(learningOutcome);
+            course3.addProgram(program);
+
+            course4.addLO(learningOutcome3);
+            course5.addLO(learningOutcome3);
+            course6.addLO(learningOutcome);
+            course7.addLO(learningOutcome2);
 
             program.addCourse(course);
+            program.addCourse(course2);
+            program.addCourse(course3);
+            program.addCourse(course4);
+            program.addCourse(course5);
+            program.addCourse(course6);
+            program2.addCourse(course7);
+            program3.addCourse(course4);
+            program3.addCourse(course5);
+            program3.addCourse(course6);
+            program4.addCourse(course4);
+            program4.addCourse(course5);
 
-            categoryRepository.save(category);
-            courseRepository.save(course);
-            learningOutcomeRepository.save(learningOutcome);
             programRepository.save(program);
-
-            categoryRepository.save(category2);
-            categoryRepository.save(category3);
-            courseRepository.save(course2);
-            courseRepository.save(course3);
             programRepository.save(program2);
             programRepository.save(program3);
+            programRepository.save(program4);
         };
     }
 }
