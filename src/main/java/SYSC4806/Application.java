5ecd62022189aaba2ece1d64926084b63efd022f
Kshamina <kshamina.ghelani@carleton.ca>
2019-03-11 18:56:12 -0400

    rm unused imports

--- src/main/java/SYSC4806/Application.java
+++ src/main/java/SYSC4806/Application.java
@@ -1,9 +1,5 @@
 package SYSC4806;
 
-import SYSC4806.Model.Category;
-import SYSC4806.Model.Course;
-import SYSC4806.Model.LearningOutcome;
-import SYSC4806.Model.Program;
 import SYSC4806.Repository.CategoryRepository;
 import SYSC4806.Repository.CourseRepository;
 import SYSC4806.Repository.LearningOutcomeRepository;
@@ -12,9 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
